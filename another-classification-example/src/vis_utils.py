d1afe1d9d5e2dc3e153bc8ef21024dbfc46c48fa
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2020-05-20 17:22:15 +0200

    Corrections in comments

--- another-classification-example/src/vis_utils.py
+++ another-classification-example/src/vis_utils.py
@@ -23,44 +23,28 @@ def log_interactive_visualisations():
 
 def _log_matplotlib_figure():
     def _scatter_hist(x, y, ax, ax_histx, ax_histy):
-        # no labels
         ax_histx.tick_params(axis="x", labelbottom=False)
         ax_histy.tick_params(axis="y", labelleft=False)
-
-        # the scatter plot:
         ax.scatter(x, y)
-
-        # now determine nice limits by hand:
         binwidth = 0.25
         xymax = max(np.max(np.abs(x)), np.max(np.abs(y)))
         lim = (int(xymax / binwidth) + 1) * binwidth
-
         bins = np.arange(-lim, lim + binwidth, binwidth)
         ax_histx.hist(x, bins=bins)
         ax_histy.hist(y, bins=bins, orientation='horizontal')
 
-    # Fixing random state for reproducibility
     np.random.seed(19680801)
-
-    # some random data
     x = np.random.randn(1000)
     y = np.random.randn(1000)
 
-    # start with a square Figure
     fig = plt.figure(figsize=(8, 8))
-
-    # Add a gridspec with two rows and two columns and a ratio of 2 to 7 between
-    # the size of the marginal axes and the main axes in both directions.
-    # Also adjust the subplot parameters for a square plot.
     gs = fig.add_gridspec(2, 2, width_ratios=(7, 2), height_ratios=(2, 7),
                           left=0.1, right=0.9, bottom=0.1, top=0.9,
                           wspace=0.05, hspace=0.05)
-
     ax = fig.add_subplot(gs[1, 0])
     ax_histx = fig.add_subplot(gs[0, 0], sharex=ax)
     ax_histy = fig.add_subplot(gs[1, 1], sharey=ax)
 
-    # use the previously defined function
     _scatter_hist(x, y, ax, ax_histx, ax_histy)
 
     # Log matplotlib figure as interactive chart in the experiment' artifacts tab.
